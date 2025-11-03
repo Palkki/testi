b01f24fe0a9bb72ce2bcf3c020309f3763059fca
Palkki <jasu.kansala@gmail.com>
2025-11-03 16:20:44 +0200

    kokonaisluku

--- testi.py
+++ testi.py
@@ -1,1 +1,10 @@
-print("Testi")
+
+while True:
+    try:
+          Morjes = int(input("Anna kokonaisluku: "))
+    except ValueError:
+         print(f"Tuo ei näytä kokonaisluvulta!\n")
+    else:
+         print(f"Annoit luvun {Morjes}")
+         break
+
